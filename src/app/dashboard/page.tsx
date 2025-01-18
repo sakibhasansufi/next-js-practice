a7477bf41bda7488927ee71f83791d48adbc2ce3
sakibhasansufi <sakibhasansufi@gmail.com>
2025-01-18 13:46:24 +0600

    register route is added

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+function BarChart() {
+    return (
+        <div>
+            <h1>Dashboard is found here</h1>
+        </div>
+    );
+};
+
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <BarChart />
+        </div>
+    );
+}
